dcccb6a6d027bf5a564b0c4e1d450cc58ce4bb3b
Cryomaniac432 <78274983+Cryomaniac432@users.noreply.github.com>
2021-01-30 19:55:48 +0100

    Initial commit

---
+++ CohTempRecRemover/Form1.Designer.cs
@@ -0,0 +1,162 @@
+﻿namespace CohTempRecRemover
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.LblTitle_Line1 = new System.Windows.Forms.Label();
+            this.LblTitle_Line2 = new System.Windows.Forms.Label();
+            this.BtnRunAndStop = new System.Windows.Forms.Button();
+            this.LblHowTo_Line1 = new System.Windows.Forms.Label();
+            this.LblHowTo_Line2 = new System.Windows.Forms.Label();
+            this.LblMessage_Line1 = new System.Windows.Forms.Label();
+            this.TimerRemoveTempRec = new System.Windows.Forms.Timer(this.components);
+            this.LblMessage_Line2 = new System.Windows.Forms.Label();
+            this.LblVersion = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // LblTitle_Line1
+            // 
+            this.LblTitle_Line1.AutoSize = true;
+            this.LblTitle_Line1.Location = new System.Drawing.Point(12, 9);
+            this.LblTitle_Line1.Name = "LblTitle_Line1";
+            this.LblTitle_Line1.Size = new System.Drawing.Size(650, 13);
+            this.LblTitle_Line1.TabIndex = 0;
+            this.LblTitle_Line1.Text = "Dieses Programm dient dem Zweck, die \"temp.rec\" von \"Company of Heroes\" automatis" +
+    "ch zu entfernen, sobald diese vorgefunden wird.";
+            // 
+            // LblTitle_Line2
+            // 
+            this.LblTitle_Line2.AutoSize = true;
+            this.LblTitle_Line2.Location = new System.Drawing.Point(12, 22);
+            this.LblTitle_Line2.Name = "LblTitle_Line2";
+            this.LblTitle_Line2.Size = new System.Drawing.Size(586, 13);
+            this.LblTitle_Line2.TabIndex = 1;
+            this.LblTitle_Line2.Text = "Diese befindet sich im jeweiligen Benutzer-Ordner unter \"Documents\\My Games\\Compa" +
+    "ny of Heroes Relaunch\\playback\".";
+            // 
+            // BtnRunAndStop
+            // 
+            this.BtnRunAndStop.BackColor = System.Drawing.Color.Lime;
+            this.BtnRunAndStop.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.BtnRunAndStop.Location = new System.Drawing.Point(15, 77);
+            this.BtnRunAndStop.Name = "BtnRunAndStop";
+            this.BtnRunAndStop.Size = new System.Drawing.Size(281, 23);
+            this.BtnRunAndStop.TabIndex = 2;
+            this.BtnRunAndStop.Text = "automatisches Entfernen der \"temp.rec\" aktivieren";
+            this.BtnRunAndStop.UseVisualStyleBackColor = false;
+            this.BtnRunAndStop.Click += new System.EventHandler(this.BtnRunAndStop_Click);
+            // 
+            // LblHowTo_Line1
+            // 
+            this.LblHowTo_Line1.AutoSize = true;
+            this.LblHowTo_Line1.Location = new System.Drawing.Point(12, 48);
+            this.LblHowTo_Line1.Name = "LblHowTo_Line1";
+            this.LblHowTo_Line1.Size = new System.Drawing.Size(456, 13);
+            this.LblHowTo_Line1.TabIndex = 3;
+            this.LblHowTo_Line1.Text = "Klicken Sie einfach auf den Knopf unten, um diese oben beschriebene Automatik zu " +
+    "aktivieren.";
+            // 
+            // LblHowTo_Line2
+            // 
+            this.LblHowTo_Line2.AutoSize = true;
+            this.LblHowTo_Line2.Location = new System.Drawing.Point(12, 61);
+            this.LblHowTo_Line2.Name = "LblHowTo_Line2";
+            this.LblHowTo_Line2.Size = new System.Drawing.Size(363, 13);
+            this.LblHowTo_Line2.TabIndex = 4;
+            this.LblHowTo_Line2.Text = "Klicken Sie erneut auf diesen Knopf um diese Automatik wieder zu stoppen.";
+            // 
+            // LblMessage_Line1
+            // 
+            this.LblMessage_Line1.AutoSize = true;
+            this.LblMessage_Line1.ForeColor = System.Drawing.Color.Red;
+            this.LblMessage_Line1.Location = new System.Drawing.Point(12, 103);
+            this.LblMessage_Line1.Name = "LblMessage_Line1";
+            this.LblMessage_Line1.Size = new System.Drawing.Size(96, 13);
+            this.LblMessage_Line1.TabIndex = 5;
+            this.LblMessage_Line1.Text = "LblMessage_Line1";
+            // 
+            // TimerRemoveTempRec
+            // 
+            this.TimerRemoveTempRec.Tick += new System.EventHandler(this.TimerRemoveTempRec_Tick);
+            // 
+            // LblMessage_Line2
+            // 
+            this.LblMessage_Line2.AutoSize = true;
+            this.LblMessage_Line2.ForeColor = System.Drawing.Color.Red;
+            this.LblMessage_Line2.Location = new System.Drawing.Point(12, 116);
+            this.LblMessage_Line2.Name = "LblMessage_Line2";
+            this.LblMessage_Line2.Size = new System.Drawing.Size(96, 13);
+            this.LblMessage_Line2.TabIndex = 6;
+            this.LblMessage_Line2.Text = "LblMessage_Line2";
+            // 
+            // LblVersion
+            // 
+            this.LblVersion.AutoSize = true;
+            this.LblVersion.Location = new System.Drawing.Point(592, 122);
+            this.LblVersion.Name = "LblVersion";
+            this.LblVersion.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.LblVersion.Size = new System.Drawing.Size(42, 13);
+            this.LblVersion.TabIndex = 7;
+            this.LblVersion.Text = "Version";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(673, 144);
+            this.Controls.Add(this.LblVersion);
+            this.Controls.Add(this.LblMessage_Line2);
+            this.Controls.Add(this.LblMessage_Line1);
+            this.Controls.Add(this.LblHowTo_Line2);
+            this.Controls.Add(this.LblHowTo_Line1);
+            this.Controls.Add(this.BtnRunAndStop);
+            this.Controls.Add(this.LblTitle_Line2);
+            this.Controls.Add(this.LblTitle_Line1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = "automatischer \"temp.rec\"-Entferner für \"Company of Heroes\"";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label LblTitle_Line1;
+        private System.Windows.Forms.Label LblTitle_Line2;
+        private System.Windows.Forms.Button BtnRunAndStop;
+        private System.Windows.Forms.Label LblHowTo_Line1;
+        private System.Windows.Forms.Label LblHowTo_Line2;
+        private System.Windows.Forms.Label LblMessage_Line1;
+        private System.Windows.Forms.Timer TimerRemoveTempRec;
+        private System.Windows.Forms.Label LblMessage_Line2;
+        private System.Windows.Forms.Label LblVersion;
+    }
+}
---
+++ CohTempRecRemover/Form1.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace CohTempRecRemover
+{
+    public partial class Form1 : Form
+    {
+        private string BtnRunAndStop_OriginalText = "";
+        private string BtnRunAndStop_TextWhenRunning = "";
+        private string TempRecDir = "";
+        private string TempRecFullPath = "";
+        private string ErrorMEssage_DirectoryNotFound = "";
+        private string ErrorMessage_DeleteFailed = "";
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            //Class Attributes
+            BtnRunAndStop_OriginalText = BtnRunAndStop.Text;
+            BtnRunAndStop_TextWhenRunning = BtnRunAndStop_OriginalText.Replace("aktivieren", "stoppen");
+            TempRecDir = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments) + "\\My Games\\Company of Heroes Relaunch\\playback";
+            TempRecFullPath = TempRecDir + "\\temp.rec";
+            ErrorMEssage_DirectoryNotFound = "Das Verzeichnis der \"temp.rec\" von \"Company of Heroes\" scheint nicht zu existieren.";
+            ErrorMessage_DeleteFailed = "Bei dem Versuch, eine Vorhandene \"temp.rec\" zu entfernen ist ein Fehler aufgetreten:";
+
+            //Labels
+            LblMessage_Line1.Text = LblMessage_Line2.Text = "";
+            LblVersion.Text = BuildVersionString_ForVersionLabel();
+        }
+
+        private void BtnRunAndStop_Click(object sender, EventArgs e)
+        {
+            LblMessage_Line1.Text = LblMessage_Line2.Text = "";
+            if (!TimerRemoveTempRec.Enabled)
+            {
+                if (Directory.Exists(TempRecDir))
+                {
+                    TimerRemoveTempRec.Start();
+                    BtnRunAndStop.BackColor = Color.Red;
+                    BtnRunAndStop.ForeColor = Color.White;
+                    BtnRunAndStop.Text = BtnRunAndStop_TextWhenRunning;
+                } else { LblMessage_Line1.Text = ErrorMEssage_DirectoryNotFound; }
+            } else
+            {
+                TimerRemoveTempRec.Stop();
+                BtnRunAndStop.BackColor = Color.Lime;
+                BtnRunAndStop.ForeColor = Color.Black;
+                BtnRunAndStop.Text = BtnRunAndStop_OriginalText;          
+            }
+        }
+
+        private void TimerRemoveTempRec_Tick(object sender, EventArgs e)
+        {
+            if (File.Exists(TempRecFullPath))
+            {
+                try { File.Delete(TempRecFullPath); } catch (Exception exc)
+                {
+                    LblMessage_Line1.Text = ErrorMessage_DeleteFailed;
+                    LblMessage_Line2.Text = exc.Message;
+                }
+            }
+        }
+
+        private string BuildVersionString_ForVersionLabel()
+        {
+            string[] currentVersionNumbers = Application.ProductVersion.Split('.');
+            string versionMajor = currentVersionNumbers.Length >= 1 ? currentVersionNumbers[0] : "?";
+            string versionMinor = currentVersionNumbers.Length >= 2 ? currentVersionNumbers[1] : "?";
+            string versionRevision = currentVersionNumbers.Length >= 3 ? currentVersionNumbers[2] : "?";
+            return "Version " + versionMajor + "." + versionMinor + "." + versionRevision;
+        }
+    }
+}
